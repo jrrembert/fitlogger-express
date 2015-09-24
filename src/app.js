6e17fe4d801fe5866d4b8d87ded0c213d4967419
J. Ryan Rembert <j.ryan.rembert@gmail.com>
2015-09-23 19:59:01 -0700

    Some initial Jade templates

--- src/app.js
+++ src/app.js
@@ -5,23 +5,25 @@ var posts = require('./mock/posts.json');
 
 var app = express();
 
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/templates');
+
 // Route for site root
 // Request = req, response = res per convention
 app.get('/', function(req, res) {
-	res.send("<h1>I (sometimes) love fitness!</h1>");
+	res.render('index.jade');
 })
 
-
-app.get('/blog', function(req, res){
-    res.send(posts);
-});
-
-app.get('/blog/:title', function(req, res){
+app.get('/blog/:title?', function(req, res) {
     var title = req.params.title;
-    var post = posts[title];
-    res.send(post);
+    if (title === undefined) {
+    	res.status(503);
+    	res.send("This page is under construction.");
+    } else {
+    	var post = posts[title] || {};
+    	res.render('post', { post: post });
+	}
 });
-
 
 app.listen(3000, function() {
 	console.log("The frontend server is running on port 3000. Press Control-C to quit.")
