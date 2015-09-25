600d2b4e4b436affba07d88d4903fe2da6fa72ee
J. Ryan Rembert <j.ryan.rembert@gmail.com>
2015-09-25 12:10:41 -0700

    Include static middleware and update templates

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ var posts = require('./mock/posts.json');
 
 var app = express();
 
+app.use('/static', express.static(__dirname + '/public'));
+
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/templates');
 
